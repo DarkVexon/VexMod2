c4a126d64f4195c7bab4dd010657e012c0eeeb7b
Vex <edanvirgo@gmail.com>
2023-08-21 14:39:39 -0700

    Just Colored ink bottle

---
+++ src/main/java/vexsQol/ColoredInkBottleMod.java
@@ -0,0 +1,18 @@
+package vexsQol;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+
+@SuppressWarnings({"unused", "WeakerAccess"})
+@SpireInitializer
+public class ColoredInkBottleMod {
+
+    public static final String modID = "coloredinkbottle";
+
+    public ColoredInkBottleMod() {
+
+    }
+
+    public static void initialize() {
+        ColoredInkBottleMod thismod = new ColoredInkBottleMod();
+    }
+}
