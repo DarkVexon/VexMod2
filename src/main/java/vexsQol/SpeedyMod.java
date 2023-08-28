15500e1553270fc14bff5708e260183376758d4b
Vex <edanvirgo@gmail.com>
2023-08-28 10:07:41 -0700

    Startup

---
+++ src/main/java/vexsQol/SpeedyMod.java
@@ -0,0 +1,20 @@
+package vexsQol;
+
+import basemod.BaseMod;
+import basemod.helpers.RelicType;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.localization.RelicStrings;
+import com.megacrit.cardcrawl.localization.UIStrings;
+
+@SuppressWarnings({"unused", "WeakerAccess"})
+@SpireInitializer
+public class SpeedyMod {
+    public SpeedyMod() {
+        //BaseMod.subscribe(this);
+    }
+
+    public static void initialize() {
+        SpeedyMod thismod = new SpeedyMod();
+    }
+}
